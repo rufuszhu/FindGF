f31ebdf74e1f3db92d93a725d814829d69bbab45
unknown <rufuszhu>
2013-07-16 23:48:09 -0600

    added signin

--- gen/com/example/findmygf/R.java
+++ gen/com/example/findmygf/R.java
@@ -182,21 +182,34 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020018;
     }
     public static final class id {
-        public static final int action_settings=0x7f050005;
+        public static final int action_forgot_password=0x7f05000c;
+        public static final int action_settings=0x7f05000d;
+        public static final int email=0x7f050008;
         public static final int hybrid=0x7f050004;
+        public static final int login=0x7f05000a;
+        public static final int login_form=0x7f050007;
+        public static final int login_status=0x7f050005;
+        public static final int login_status_message=0x7f050006;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
+        public static final int password=0x7f050009;
         public static final int satellite=0x7f050002;
+        public static final int sign_in_button=0x7f05000b;
         public static final int terrain=0x7f050003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f090000;
+        public static final int login=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class string {
+        public static final int action_forgot_password=0x7f06001e;
         public static final int action_settings=0x7f060016;
+        public static final int action_sign_in_register=0x7f06001c;
+        public static final int action_sign_in_short=0x7f06001d;
         public static final int app_name=0x7f060015;
         /**   Title for notification shown when GooglePlayServices is unavailable [CHAR LIMIT=70] 
          */
@@ -262,6 +275,10 @@ containing a value of this type.
         /**  Long form sign-in button text [CHAR LIMIT=30] 
          */
         public static final int common_signin_button_text_long=0x7f06000e;
+        public static final int error_field_required=0x7f060023;
+        public static final int error_incorrect_password=0x7f060022;
+        public static final int error_invalid_email=0x7f060020;
+        public static final int error_invalid_password=0x7f060021;
         public static final int hello_world=0x7f060017;
         /**  Location client code resources (prefix with location_client) 
 
@@ -274,16 +291,25 @@ containing a value of this type.
         public static final int location_client_ulr_inactive_age_under_13=0x7f060012;
         public static final int location_client_ulr_inactive_age_unknown=0x7f060013;
         public static final int location_client_ulr_inactive_unknown_restriction=0x7f060014;
+        public static final int login_progress_signing_in=0x7f06001f;
+        public static final int progress_message=0x7f060018;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f06001a;
+        public static final int prompt_password=0x7f06001b;
+        public static final int title_activity_login=0x7f060019;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -302,6 +328,7 @@ containing a value of this type.
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f080001;
+        public static final int LoginFormContainer=0x7f080002;
     }
     public static final class styleable {
         /** Attributes that can be used with a MapAttrs.
